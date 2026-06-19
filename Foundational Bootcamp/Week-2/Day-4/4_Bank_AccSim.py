3381eece40f40034c8285d7de0e378c276d4501b
Farhan0386 <farhan03860@gmail.com>
2026-06-19 15:02:05 +0530

    refactor: improve formatting and error handling in Account class methods

--- Foundational Bootcamp/Week-2/Day-4/4_Bank_AccSim.py
+++ Foundational Bootcamp/Week-2/Day-4/4_Bank_AccSim.py
@@ -1,28 +1,32 @@
 class Account:
-    def __init__(self, money):
-        self.money = money
+    def __init__(self,money):
+        self.money=money
+    def deposit(self,amount):
+        try:
+            if amount<=0:
+                raise ValueError("Invalid amount")
 
-    def deposit(self, amount):
-        if amount <= 0:
-            print("Invalid amount")
-            return
-        self.money += amount
-        print("Deposited ₹", amount)
+            self.money+=amount
+            print("Deposited ₹",amount)
 
-    def withdraw(self, amount):
-        if amount <= 0:
-            print("Invalid amount")
-            return
-        if amount > self.money:
-            print("Insufficient balance")
-            return
-        self.money -= amount
-        print("Withdrawn ₹", amount)
-
+        except ValueError as e:
+            print(e)
+    def withdraw(self,amount):
+        try:
+            if amount<=0:
+                raise ValueError("Invalid amount")
+            if amount>self.money:
+                raise Exception("Insufficient balance")
+            self.money-=amount
+            print("Withdrawn ₹",amount)
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(e)
     def balance(self):
-        print("Current Balance =", self.money)
+        print("Current Balance =",self.money)
 
-user_account = Account(1000)
+user_account=Account(1000)
 user_account.deposit(500)
 print("----------------------------------")
 user_account.balance()
